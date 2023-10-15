7411680dff51aed31c27d019919ef9692974660a
ogofoluwa <olusway@gmail.com>
2023-10-15 18:19:10 +0000

    fixes made for test

--- 0-hsee.c
+++ 0-hsee.c
@@ -2,32 +2,34 @@
 
 /**
   *handle_see - handles other specifiers
+  *@format: format string
+  *@args: stored arg vars
+  *Return: int
   */
 int handle_see(const char *format, va_list args)
 {
 	char cara, *str;
 	int ctr = 0, i = 0;
 
-
 	if (format[i] == 'c')
- 	{
- 		cara = va_arg(args, int);
+	{
+		cara = va_arg(args, int);
 		_putchar(cara);
 		ctr++;
 		i++;
- 	}
- 	else if (format[i] == 's')
-  	{
- 		str = va_arg(args, char *);
+	}
+	else if (format[i] == 's')
+	{
+		str = va_arg(args, char *);
 		write(1, str, strlen(str));
 		ctr += strlen(str);
 		i++;
 	}
- 	else if (format[i] == '%')
- 	{
- 		 _putchar(format[i]);
-		 ctr++;
-		 i++;
+	else if (format[i] == '%')
+	{
+		_putchar(format[i]);
+		ctr++;
+		i++;
 	}
 	else
 	{
